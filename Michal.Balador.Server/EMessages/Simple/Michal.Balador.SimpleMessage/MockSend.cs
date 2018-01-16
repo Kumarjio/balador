533575b89323196712b35b66de32daae848048b1
lior grossman <lior.grosman@gmail.com>
2018-01-16 11:44:05 +0200

    OK

--- Michal.Balador.Server/EMessages/Simple/Michal.Balador.SimpleMessage/MockSend.cs
+++ Michal.Balador.Server/EMessages/Simple/Michal.Balador.SimpleMessage/MockSend.cs
@@ -8,39 +8,7 @@ using Michal.Balador.Contracts.DataModel;
 
 namespace Michal.Balador.SimpleMessage
 {
-   public class SenderMessagesFactory
-    {
-        public async Task<Response<MockSend>> ConnectAndLogin(string user, string pws)
-        {
-            var tcs = new TaskCompletionSource<Response<MockSend>>();
-            try
-            {
-                SocketClientTest  test = new SocketClientTest(user, pws);
-                test.Connect();
-
-                test.OnLoginSuccess += (a, b) =>
-                {
-                    Console.WriteLine("data ok={0}", a);
-                    string m = "test me...";
-                    test.SendData(Encoding.ASCII.GetBytes(m));
-                    tcs.SetResult(new Response<MockSend> { IsError = false, Result=new MockSend(test) });
-                };
-                test.OnLoginFailed += (a) =>
-                {
-
-                    Console.WriteLine("data error={0}", a);
-                    tcs.SetResult(new Response<MockSend> { IsError = true,Message=a });
-                };
-                test.Login(true);
-            }
-            catch (Exception ee)
-            {
-                tcs.SetException(ee);
-            }
-
-            return await tcs.Task;
-        }
-    }
+   
 
     public class MockSend : SenderMessages
     {
---
+++ Michal.Balador.Server/EMessages/Simple/Michal.Balador.SimpleMessage/SenderMessagesFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using Michal.Balador.Contracts.DataModel;
+
+namespace Michal.Balador.SimpleMessage
+{
+    public class SenderMessagesFactory
+    {
+        public async Task<Response<MockSend>> ConnectAndLogin(string user, string pws)
+        {
+            var tcs = new TaskCompletionSource<Response<MockSend>>();
+            try
+            {
+                SocketClientTest test = new SocketClientTest(user, pws);
+                test.Connect();
+
+                test.OnLoginSuccess += (a, b) =>
+                {
+                    Console.WriteLine("data ok={0}", a);
+                    string m = "test me...";
+                    test.SendData(Encoding.ASCII.GetBytes(m));
+                    tcs.SetResult(new Response<MockSend> { IsError = false, Result = new MockSend(test) });
+                };
+                test.OnLoginFailed += (a) =>
+                {
+
+                    Console.WriteLine("data error={0}", a);
+                    tcs.SetResult(new Response<MockSend> { IsError = true, Message = a });
+                };
+                test.Login(true);
+            }
+            catch (Exception ee)
+            {
+                tcs.SetException(ee);
+            }
+
+            return await tcs.Task;
+        }
+    }
+}
